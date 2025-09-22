98153671cbf28fc8d9c447f649de441545799190
Melody Ness Ecarma <ecarmamelody@gmail.com>
2025-09-23 01:12:53 +0800

    kini?

--- src/boardarray.h
+++ src/boardarray.h
@@ -14,30 +14,12 @@ class BoardArray : public Board {
         }
 
         void add(Entry* entry) {
-            // TODO: IMPLEMENT THIS FUNCTION
-            // ALGORITHM IS PROVIDED IN INSTRUCTIONS.TXT
-            // a. Traverse the array up until the last element
-            // b. Compare (using the bool compare() function of Entry) the `entry` to be added to the `existing entry` currently traversed.
-            // c. To get the current entry, you can access it via `array[i * sizeof(Entry)]` or `*(array + i * sizeof(Entry)). I will discuss first why is this the case.
-            // d. If `entry` to be added is indeed greater than the existing one, `SHIFT RIGHT`
-            //e. After shift right, add the new entry to the correct position.
-            for(int i = 0; i<index; i++){
-                array[i].compare();
+            if (index < SIZE) {
+                array[index] = *entry;
+                index++;
             }
-
-            bool compare(Entry* entry){
-                Entry existing = array[i];
-                existing.compare();
-
-                if(entry ){
-
-                }else{
-                    cout 
-                }
-            }
-
             return;
-        }
+            }
 
         void print() {
             for (int i = 0; i < index; i++) {
